0ebc2a5f4ab8f0c1b9685ca766a628322b02ff25
dimansion <dimansional@gmail.com>
2021-10-04 15:28:09 +0700

    [IMP] update license

--- mjt_mass_recompute/__manifest__.py
+++ mjt_mass_recompute/__manifest__.py
@@ -10,7 +10,7 @@
     """,
 
     'author': "MJT",
-    'license':'AGPL-3.0',
+    'license':'AGPL-3',
     'website': "http://www.metrocomjaddi.com",
     'category': 'Payroll',
     'version': '0.1',
