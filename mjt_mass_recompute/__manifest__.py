a39eca8198b142a2560a5beed6ef66737f3e971e
dimansion <dimansional@gmail.com>
2021-10-04 11:44:55 +0700

    [ADD] add mass recompute

---
+++ mjt_mass_recompute/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Re-compute Mass Payslip",
+
+    'summary': """
+        Re-compute mass payslip""",
+
+    'description': """
+        To re-compute mass payslip at the same time
+    """,
+
+    'author': "MJT",
+    'website': "http://www.metrocomjaddi.com",
+    'category': 'Payroll',
+    'version': '0.1',
+
+    'depends': ['base', 'hr_payroll'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'wizard/compute_hr_payslip_wiz_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
